437df1397c2010d1d5d75dcb26ba0a6011735220
abrp <abrp@abrp.dk>
2016-04-09 00:00:44 +0200

    Spawner added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ public class Spawner : MonoBehaviour {
 	
 	[SerializeField] private GameObject[] m_items;
 	[SerializeField] private float m_spawnRate = 0.5f;
-	private float m_nextSpawnTime;
+	private float m_nextSpawnTime = 0;
 
 	void Start () {
 		
@@ -13,8 +13,9 @@ public class Spawner : MonoBehaviour {
 	
 
 	void Update () {
-		if (m_nextSpawnTime > Time.time) {
-
+		if (m_nextSpawnTime < Time.time) {
+			Instantiate (m_items [0], transform.position, Quaternion.identity);
+			m_nextSpawnTime = Time.time + m_spawnRate;
 		}
 	}
 }
