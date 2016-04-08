79af7ec66d8882369667e813c2d556668b6ec268
abrp <abrp@abrp.dk>
2016-04-08 22:25:20 +0200

    Camera Container prefab and movetest

---
+++ Assets/Scripts/Abrp Utils/MoveTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTest : MonoBehaviour {
+
+	[SerializeField] float m_speed = 1;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		transform.Translate(Vector3.forward * Time.deltaTime * m_speed);
+	}
+}
