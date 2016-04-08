03fa0fca44185688f56b7affe9ceb90d73e3ef79
Casper Fabricius <me@casperfabricius.com>
2016-04-09 00:18:01 +0200

    Pushable script added

---
+++ Assets/Scripts/PushableItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(Rigidbody), typeof(VRInteractiveItem))]
+public class PushableItem : MonoBehaviour {
+
+  private Rigidbody m_Rigibody;
+  private VRInteractiveItem m_InteractiveItem;
+
+  [SerializeField] private float m_Speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+    m_Rigibody = this.GetComponent<Rigidbody>();
+    m_InteractiveItem = this.GetComponent<VRInteractiveItem>();
+    m_InteractiveItem.OnClick += this.Push;
+	}
+    
+  void Push() {
+    m_Rigibody.isKinematic = false;
+    m_Rigibody.velocity = new Vector3(0, 0, m_Speed);
+  }
+}
