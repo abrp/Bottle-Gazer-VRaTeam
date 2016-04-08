2580c9f665b55b6767b01dd1ea7f0355e1ff0d46
abrp <abrp@abrp.dk>
2016-04-08 21:39:30 +0200

    base

---
+++ Assets/Scripts/Abrp Utils/Gaze.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/***
+ * Script for gazing at objects
+ * by Anders Bjørn Rørbæk Pedersen
+ ***/
+
+public class Gaze : MonoBehaviour {
+
+	[SerializeField] private Reticle m_reticle;
+
+	// debug rays
+	private bool m_showDebugRay = true;
+	private float m_rayLenght = 100;
+
+	void Awake () {
+		if (m_reticle != null) {
+			m_reticle = Instantiate (m_reticle, new Vector3 (0, 0, 0), Quaternion.identity) as Reticle;
+		}
+	}
+
+	void Update () {
+		EyeRaycast ();
+	}
+    
+	private void EyeRaycast(){
+
+		// start position for raycast
+		Vector3 startPosition = transform.position; 
+
+		// direction for raycast
+		Vector3 direction = transform.forward;
+
+		// the ray
+		Ray ray = new Ray (startPosition, direction);
+
+		// debug ray in editor mode
+		if (m_showDebugRay) {
+			Debug.DrawRay (ray.origin, ray.direction * m_rayLenght, Color.red);
+		}
+
+		// raycasthit information
+		RaycastHit hit;
+
+		if (Physics.Raycast(startPosition,direction * m_rayLenght, out hit,m_rayLenght)){
+			m_reticle.SetPosition (hit.point);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Abrp Utils/MoveableArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class MoveableArea : MonoBehaviour {
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	[SerializeField] private MoveController m_MoveController;
+
+	private bool m_GazeOver;
+
+	private void Awake(){
+		if (m_InteractiveItem == null) {
+			m_InteractiveItem = gameObject.AddComponent<VRInteractiveItem> ();
+		}
+    m_MoveController = Camera.main.GetComponentInParent<MoveController> ();
+	}
+
+	private void OnEnable(){
+		m_InteractiveItem.OnClick += HandleClick;
+    //m_InteractiveItem.OnDoubleClick += HandleClick;
+    //m_InteractiveItem.OnUp += HandleClick;
+	}
+
+	private void OnDisable(){
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+	private void HandleClick(){
+    m_MoveController.MoveToGaze ();
+	}
+}
