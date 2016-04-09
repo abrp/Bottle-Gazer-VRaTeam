32b8198f1aac9e96312d41ba0b87ce9f7d5a80b8
Ulf R. Pedersen <ulf@urp.dk>
2016-04-09 12:11:11 +0200

    update af bump

--- Assets/Scripts/BumpyUpDown.cs
+++ Assets/Scripts/BumpyUpDown.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class BumpyUpDown : MonoBehaviour {
 
-	[SerializeField] private float m_Bumpyness = 1f;
+	[SerializeField] private float m_Bumpyness = 0.02f;
+	[SerializeField] private float m_UpDownSkin = 0.3f;
 
 	private float lowerBound;
 	private float upperBound;
-	private float Speed;
+	private float force;
+	private Vector3 velocity = new Vector3 (0, 0, 0);	// private float Speed;
 
 	private Rigidbody m_Rigidbody;
-
 	private Collider m_Collider;
 
 	// Use this for initialization
@@ -19,11 +20,15 @@ public class BumpyUpDown : MonoBehaviour {
 		m_Rigidbody = GetComponent<Rigidbody>();
 
 		lowerBound = transform.position.y;
-		upperBound = transform.position.y+0.01f;
-		Speed = 0;
+		upperBound = transform.position.y+m_UpDownSkin;
 
 		Debug.Log (" collider = " + m_Collider.transform.position.y);
 		Debug.Log (" transform = " + transform.position.y);
+		Debug.Log (" lowerBound = " + lowerBound);
+		Debug.Log (" lowerBound = " + upperBound);
+
+		force = 0f;
+		velocity = new Vector3 (0, 0, 0);
 
 	}
 	
@@ -31,18 +36,19 @@ public class BumpyUpDown : MonoBehaviour {
 	void Update () {
 
 		if (m_Rigidbody.isKinematic) {
-			
-			Vector3 velocity = new Vector3 (0, 0, 0);
 
 			// Add bumpyness
-			velocity.y = m_Bumpyness * Random.Range (-1f, 1f);
-			if (transform.position.y < lowerBound)
-				velocity.y = m_Bumpyness * Random.Range (0, 1f);
-			else if (transform.position.y > upperBound)
-				velocity.y = m_Bumpyness * Random.Range (-1f, 0);
-			else
-				velocity.y = m_Bumpyness * Random.Range (-1f, 1f);
+			if (transform.position.y < lowerBound) {
+				force = Random.Range (0, m_Bumpyness);
+				velocity.y = 0f;
+			} else if (transform.position.y > upperBound) {
+				force = Random.Range (-m_Bumpyness, 0);
+				velocity.y = 0f;
+			} else {
+				force = Random.Range (-m_Bumpyness, m_Bumpyness);
+			}
 			// Move the bottle
+			velocity.y += force*Time.deltaTime;
 			transform.Translate (velocity * Time.deltaTime);
 		}
 	}
