f85b37b0b45e956dc99e5702acc6feaf5016df9c
Casper Fabricius <me@casperfabricius.com>
2016-04-09 13:35:27 +0200

    Added Scoring Manager and right and wrong item scripts for tagging

---
+++ Assets/Scripts/ScoringManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ScoringManager : MonoBehaviour {
+
+  private int m_CurrentScore;
+
+  [SerializeField] private int m_InitialScore = 1000;
+
+  public static ScoringManager instance = null; //Static instance of ScoringManager which allows it to be accessed by any other script.
+  public event Action OnScoreChanged; // Called when the score changes
+
+  //Awake is always called before any Start functions
+  void Awake()
+  {
+    //Check if instance already exists
+    if (instance == null)
+
+      //if not, set instance to this
+      instance = this;
+
+    //If instance already exists and it's not this:
+    else if (instance != this)
+
+      //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a ScoringManager.
+      Destroy(gameObject);    
+
+    //Sets this to not be destroyed when reloading scene
+    DontDestroyOnLoad(gameObject);
+  }
+
+	// Use this for initialization
+	void Start () {
+    m_CurrentScore = m_InitialScore;
+	}
+
+  public void ApplyScore(int score) {
+    m_CurrentScore += score;
+
+    if (OnScoreChanged != null)
+      OnScoreChanged();
+  }
+
+  public int GetCurrentScore() {
+    return m_CurrentScore;
+  }
+}
---
+++ Assets/Scripts/WrongItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Tags an item on the belt as NOT being supposed to pass through
+public class WrongItem : MonoBehaviour {
+
+  [SerializeField] private int m_Score = -10;
+
+  public int GetScore() {
+    return m_Score;
+  }
+}
