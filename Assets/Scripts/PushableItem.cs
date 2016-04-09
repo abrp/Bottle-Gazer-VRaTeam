cb3438103281506183bc6da840995f258088908d
Casper Fabricius <me@casperfabricius.com>
2016-04-09 19:45:36 +0200

    Added bottle collision sounds

--- Assets/Scripts/PushableItem.cs
+++ Assets/Scripts/PushableItem.cs
@@ -10,6 +10,9 @@ public class PushableItem : MonoBehaviour {
   private VRInteractiveItem m_InteractiveItem;
 
   [SerializeField] private float m_Force = 100f;
+  [SerializeField] private AudioClip[] m_CollisionSounds;
+  [SerializeField, Range(0.5f, 3f)] private float m_MinCollisionPitch = 0.9f;
+  [SerializeField, Range(0.5f, 3f)] private float m_MaxCollisionPitch = 1.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +22,18 @@ public class PushableItem : MonoBehaviour {
     m_InteractiveItem.OnClick += this.Push;
 	}
     
-  void Push() {
+  void Push() {    
     m_Rigibody.isKinematic = false;
     var direction = this.transform.forward * m_Force;
     direction = m_sceneCamera.transform.TransformDirection(direction);
     m_Rigibody.AddForce(direction);
 
     ScoringManager.instance.PlayerRemovedItem(this.gameObject);
+  }
+
+  void OnCollisionEnter() {
+    var randomClipIndex = Random.Range(0, m_CollisionSounds.Length - 1);
+    var randomClip = m_CollisionSounds[randomClipIndex];
+    SoundManager.instance.Play(randomClip, this.transform.position, m_MinCollisionPitch, m_MaxCollisionPitch);
   }
 }
