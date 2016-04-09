d1f6eace570b56ed6a15b27e9195cce587cd6ea5
abrp <abrp@abrp.dk>
2016-04-09 13:50:11 +0200

    Monitor Screen text done and texttyper

---
+++ Assets/Scripts/TextTyper.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyper : MonoBehaviour {
+	
+	public float letterPause = 0.1f;
+	public float pauseTime = 0.1f;
+	private AudioSource audioSource;
+	public AudioClip writerSound;
+	public AudioClip noticationStart;
+	public AudioClip noticationEnd;
+	public char pauseChar;
+	public char clearScreen;
+	public Animator animator;
+	private bool isActive = true;
+
+	//public string message;
+	public Text textComp;
+	public string message;
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+		TypeTextOnScreen (2);
+	}
+
+	void Update(){
+
+	}
+
+	// Use this for initialization
+	public void TypeTextOnScreen (float delay) {
+		if (!isActive) {
+			isActive = true;
+		}
+
+		textComp = GetComponent<Text>();
+		message = textComp.text;
+		textComp.text = "";
+		StartCoroutine(TypeText (message, delay));
+	}
+	
+	IEnumerator TypeText (string message, float delay) {
+		yield return new WaitForSeconds (delay);
+		audioSource.clip = noticationStart;
+		audioSource.Play ();
+		yield return new WaitForSeconds (0.5f);
+		foreach (char letter in message.ToCharArray()) {
+			audioSource.clip = writerSound;
+			if (letter == pauseChar) {
+				yield return new WaitForSeconds (pauseTime);
+				//audioSource.clip = noticationStart;
+			}else if(letter == clearScreen){
+				textComp.text = "";
+				audioSource.clip = noticationEnd;
+				audioSource.Play ();
+			} else {
+				textComp.text += letter;
+				if(audioSource != null){
+					audioSource.Play ();
+				}else{
+					Debug.LogError("AudioClip");
+				}
+			}
+			yield return new WaitForSeconds (letterPause);
+		}
+		yield return new WaitForSeconds (0.5f);
+		audioSource.clip = noticationEnd;
+		audioSource.Play ();
+	}
+}
+
