51f48ce7a9411bc6b6d1132a8b082c56cf58e42e
Ulf R. Pedersen <ulf@urp.dk>
2016-04-09 11:16:27 +0200

    new bottle prefab, BottleUrp

---
+++ Assets/Scripts/BumpyUpDown.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumpyUpDown : MonoBehaviour {
+
+	[SerializeField] private float m_Bumpyness = 1f;
+
+	private float lowerBound;
+	private float upperBound;
+	private float Speed;
+
+	private Rigidbody m_Rigidbody;
+
+	private Collider m_Collider;
+
+	// Use this for initialization
+	void Start () {
+		m_Collider = GetComponent<Collider> ();
+		m_Rigidbody = GetComponent<Rigidbody>();
+
+		lowerBound = transform.position.y;
+		upperBound = transform.position.y+0.01f;
+		Speed = 0;
+
+		Debug.Log (" collider = " + m_Collider.transform.position.y);
+		Debug.Log (" transform = " + transform.position.y);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (m_Rigidbody.isKinematic) {
+			
+			Vector3 velocity = new Vector3 (0, 0, 0);
+
+			// Add bumpyness
+			velocity.y = m_Bumpyness * Random.Range (-1f, 1f);
+			if (transform.position.y < lowerBound)
+				velocity.y = m_Bumpyness * Random.Range (0, 1f);
+			else if (transform.position.y > upperBound)
+				velocity.y = m_Bumpyness * Random.Range (-1f, 0);
+			else
+				velocity.y = m_Bumpyness * Random.Range (-1f, 1f);
+			// Move the bottle
+			transform.Translate (velocity * Time.deltaTime);
+		}
+	//}
+}
--- Assets/Scripts/ConveyorBeltMovement.cs
+++ Assets/Scripts/ConveyorBeltMovement.cs
@@ -4,25 +4,28 @@ using System.Collections;
 public class ConveyorBeltMovement : MonoBehaviour {
 
 	[SerializeField] private float m_speed = 0.1f;
-
+	private Rigidbody m_Rigidbody;
 
 	void Start () {
-
+		m_Rigidbody = GetComponent<Rigidbody>();
 	}
 
 	void Update () {
-		Vector3 velocity = new Vector3(0,0,0);
-		if (transform.position.z < 0 && transform.position.x < 0) {
-			velocity.z += 1;
-		} else if (transform.position.z < 0 && transform.position.x > 0) {
-			velocity.z -= 1;
-		} else {
-			velocity.x = transform.position.z;
-			velocity.z = -transform.position.x;
-		}
+		if (m_Rigidbody.isKinematic) {
+			Vector3 velocity = new Vector3 (0, 0, 0);
+			if (transform.position.z < 0 && transform.position.x < 0) {
+				velocity.z += 1;
+			} else if (transform.position.z < 0 && transform.position.x > 0) {
+				velocity.z -= 1;
+			} else {
+				velocity.x = transform.position.z;
+				velocity.z = -transform.position.x;
+			}
 
-		velocity = velocity.normalized * m_speed;
+			velocity = velocity.normalized * m_speed;
 			
-		transform.Translate(velocity * Time.deltaTime);
+			transform.Translate (velocity * Time.deltaTime);
+		}
 	}
+
 }
