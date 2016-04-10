8890035030be0564317c2412356a74036a9bf6e0
abrp <abrp@abrp.dk>
2016-04-10 09:56:53 +0200

    Destroy - null pointer check for level manager

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -6,7 +6,9 @@ public class Destroyer : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
     if(other.GetComponent<VRInteractiveItem>()) {
       ScoringManager.instance.ItemReachedEndOfBelt(other.gameObject);
-      LevelManager.instance.BeltItemRemoved(other.gameObject.GetComponent<BeltItem>());
+	if (LevelManager.instance != null) {
+      	LevelManager.instance.BeltItemRemoved(other.gameObject.GetComponent<BeltItem>());
+	}
       Destroy(other.gameObject);
     }
 	}
